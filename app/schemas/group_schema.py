5f3239285768c6ebbc56ba980263c3cb3f97653d
winterkid <137682088+winterkid2000@users.noreply.github.com>
2025-04-22 14:33:41 +0900

    Update group_schema.py

--- app/schemas/group_schema.py
+++ app/schemas/group_schema.py
@@ -1,1 +1,19 @@
+from pydantic import BaseModel
+from datetime import datetime
+from typing import List
+
+class GroupCreate(BaseModel):
+    departure: str
+    destination: str
+    request_ids: List[int]
+
+class GroupOut(BaseModel):
+    id: int
+    departure: str
+    destination: str
+    created_at: datetime
+    request_ids: List[int]
+
+    class Config:
+        orm_mode = True
 
