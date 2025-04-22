ea0eca12eead3e450459df48532e5f6814441af9
winterkid <137682088+winterkid2000@users.noreply.github.com>
2025-04-22 14:33:15 +0900

    Update request_schema.py

--- app/schemas/request_schema.py
+++ app/schemas/request_schema.py
@@ -1,1 +1,19 @@
+from pydantic import BaseModel, EmailStr
+from datetime import datetime
+
+class RideRequestCreate(BaseModel):
+    email: EmailStr
+    departure: str
+    destination: str
+
+class RideRequestOut(BaseModel):
+    id: int
+    email: EmailStr
+    departure: str
+    destination: str
+    is_matched: bool
+    created_at: datetime
+
+    class Config:
+        orm_mode = True
 
