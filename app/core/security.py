b4843ff47a0dc0806d2a278ee9570afbac85a827
winterkid <137682088+winterkid2000@users.noreply.github.com>
2025-04-22 14:35:39 +0900

    Create security.py

---
+++ app/core/security.py
@@ -0,0 +1,12 @@
+from datetime import datetime, timedelta
+from jose import jwt, JWTError
+
+SECRET_KEY = "yontak_secret_key"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 60 * 24 * 7  # 7일
+
+def create_access_token(data: dict, expires_delta: timedelta = None):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + (expires_delta or timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES))
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
