c7ff2605efda1da0597036b754a5103fed566c5b
winterkid <137682088+winterkid2000@users.noreply.github.com>
2025-04-22 15:03:27 +0900

    Update ride_group.py

--- app/models/ride_group.py
+++ app/models/ride_group.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, Boolean
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 from datetime import datetime
@@ -12,6 +12,7 @@ class RideGroup(Base):
     departure = Column(String, nullable=False)
     destination = Column(String, nullable=False)
     created_at = Column(DateTime, default=datetime.utcnow)
+    is_completed = Column(Boolean, default=False)  #이동 완료 여부
 
     requests = relationship("RideRequestInGroup", back_populates="group")
 
